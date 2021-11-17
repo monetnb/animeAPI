5a40cb53fd33852408ba2ca916b82b2bb8a764b2
Monet Babazadeh <73405426+monetnb@users.noreply.github.com>
2021-11-17 09:39:36 -0800

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-async function AnimeList() {
-  const res = await fetch('https://api.jikan.moe/v3/search/anime?q=nar&page=1');
-  console.log(res);
-  const data = await res.json();
-  console.log(data);
-}
-
-AnimeList();
